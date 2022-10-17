8184c82ff8bd148704e576fa4022d061c92267b4
DAZELPRO <dazelpro@gmail.com>
2022-10-17 10:31:09 +0700

    #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'belajar';
+  name = 'Zeldianto';
+
+  onClick() {
+    this.name = 'Zel';
+  }
 }
